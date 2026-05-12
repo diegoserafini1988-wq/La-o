6804aef1e9b6860f1ebeaa767decbbda74e58018
diegoserafini1988-wq <diegoserafini1988@gmail.com>
2026-05-12 01:15:30 -0300

    Refactor like function to handle user authentication

--- main.js
+++ main.js
@@ -1,50 +1,19 @@
-let likedProfiles = [];
+let carregou = false;
 
-function like() {
-    const card = document.getElementById("card");
+firebase.auth().onAuthStateChanged(user => {
 
-    card.style.transform = "translateX(500px) rotate(30deg)";
+  if (!carregou) {
+    carregou = true;
 
-    setTimeout(() => {
-        checkMatch();
-        nextProfile();
-    }, 300);
-}
-
-function dislike() {
-    const card = document.getElementById("card");
-
-    card.style.transform = "translateX(-500px) rotate(-30deg)";
-
-    setTimeout(() => {
-        nextProfile();
-    }, 300);
-}
-
-function nextProfile() {
-    const profiles = [
-        {nome: "Ana, 25", img: "https://randomuser.me/api/portraits/women/44.jpg"},
-        {nome: "Julia, 22", img: "https://randomuser.me/api/portraits/women/65.jpg"},
-        {nome: "Marina, 27", img: "https://randomuser.me/api/portraits/women/12.jpg"}
-    ];
-
-    let random = profiles[Math.floor(Math.random() * profiles.length)];
-
-    const card = document.getElementById("card");
-
-    card.style.transform = "none";
-
-    card.innerHTML = `
-        <img src="${random.img}">
-        <h3>${random.nome}</h3>
-        <p>Conexão especial 💘</p>
-    `;
-}
-
-function checkMatch() {
-    let matchChance = Math.random();
-
-    if (matchChance > 0.7) {
-        alert("💘 Deu Match!");
+    if (user) {
+      if (window.location.pathname.includes("login.html")) {
+        window.location.href = "app.html";
+      }
+    } else {
+      if (!window.location.pathname.includes("login.html")) {
+        window.location.href = "login.html";
+      }
     }
-}
+  }
+
+});
